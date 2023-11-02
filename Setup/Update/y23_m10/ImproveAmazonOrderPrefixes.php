bc1ba3d3e96aa9cd7d4c80082195685972ef8561
M2E Pro <m2epro@users.noreply.github.com>
2023-11-02 19:09:42 +0000

    1.50.1 (FINAL RELEASE)

--- Setup/Update/y23_m10/ImproveAmazonOrderPrefixes.php
+++ Setup/Update/y23_m10/ImproveAmazonOrderPrefixes.php
@@ -15,9 +15,9 @@ class ImproveAmazonOrderPrefixes extends \Ess\M2ePro\Model\Setup\Upgrade\Entity\
         while ($row = $query->fetch()) {
             $data = json_decode($row['magento_orders_settings'], true);
             $generalPrefix = $data['number']['prefix']['prefix'] ?? '';
-            $data['number']['prefix']['afn-prefix'] = $generalPrefix . $data['number']['prefix']['afn-prefix'] ?? '';
-            $data['number']['prefix']['prime-prefix'] = $generalPrefix . $data['number']['prefix']['prime-prefix'] ?? '';
-            $data['number']['prefix']['b2b-prefix'] = $generalPrefix . $data['number']['prefix']['b2b-prefix'] ?? '' ;
+            $data['number']['prefix']['afn-prefix'] = $generalPrefix . ($data['number']['prefix']['afn-prefix'] ?? '');
+            $data['number']['prefix']['prime-prefix'] = $generalPrefix . ($data['number']['prefix']['prime-prefix'] ?? '');
+            $data['number']['prefix']['b2b-prefix'] = $generalPrefix . ($data['number']['prefix']['b2b-prefix'] ?? '');
 
             $this->getConnection()->update(
                 $tableName,
