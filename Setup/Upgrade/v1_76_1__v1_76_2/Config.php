770f958fa0f8dbf30711332a5b41268660a3b558
M2E Pro <m2epro@users.noreply.github.com>
2025-03-27 08:05:09 +0000

    1.76.2 (FINAL RELEASE)

---
+++ Setup/Upgrade/v1_76_1__v1_76_2/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ess\M2ePro\Setup\Upgrade\v1_76_1__v1_76_2;
+
+class Config extends \Ess\M2ePro\Model\Setup\Upgrade\Entity\AbstractConfig
+{
+    public function getFeaturesList(): array
+    {
+        return [];
+    }
+}
